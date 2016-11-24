625aab8221c361275b1e98b406b164fd65043262
masterUNG <phrombutr@gmail.com>
2016-11-24 15:43:55 +0700

    สำหรับต้น

---
+++ app/src/main/java/rtc/ariya/supakrit/jigsaw/ChooseStructor.java
@@ -0,0 +1,14 @@
+package rtc.ariya.supakrit.jigsaw;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChooseStructor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_structor);
+    }   // Main Method
+
+}   // Main Class
